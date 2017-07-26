83d36b719a6ef9dd79df669a87d2e12881d33644
Jaroslaw Zawila <jaroslaw.zawila@ovoenergy.com>
2017-07-26 08:21:54 +0100

    Add acceptance test

---
+++ test/acceptance/HealthcheckITest.scala
@@ -0,0 +1,36 @@
+package acceptance
+
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import play.api.libs.json.JsValue
+import play.api.test.Helpers._
+import play.api.test._
+
+class HealthcheckITest extends PlaySpec with GuiceOneServerPerSuite {
+
+  "Ping" must {
+    "returns pong" in {
+      val response = await(WsTestClient.withClient(client => {
+        client.url(s"http://localhost:$port/ping").execute()
+      }))
+
+      response.status must be(OK)
+      response.body must be("pong")
+    }
+  }
+
+  "Healthcheck" must {
+    "be healthy" in {
+      val response = await(WsTestClient.withClient(client => {
+        client.url(s"http://localhost:$port/healthcheck").execute()
+      }))
+
+      response.status must be(OK)
+      val json = response.body[JsValue]
+      (json \ "name").get.toString() must be("\"play-with-paly\"")
+      (json \ "version").get.toString() must be("\"0.1-SNAPSHOT\"")
+      (json \ "isHealthy").get.validate[Boolean].get must be(true)
+    }
+  }
+
+}
